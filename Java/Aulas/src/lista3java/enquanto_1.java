5b8b64966a3a4069a8570eec0a0bbfab6d3f1310
LucasAnseloni <lucas.anseloni@outlook.com>
2020-12-09 23:01:11 -0300

    java

--- Java/Aulas/src/lista3java/enquanto_1.java
+++ Java/Aulas/src/lista3java/enquanto_1.java
@@ -2,53 +2,36 @@ package lista3java;
 
 import java.util.Scanner;
 
-public class enquanto_1 
-{
+public class enquanto_1 {
 
-	public static void main(String[] args) 
-	{
-		Scanner leia = new Scanner (System.in);
-		
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+
 		double numeroDigitado = 0;
-		double soma =0 ;
-		double media ;
-		int contador =0;
-		
-		
-		
-		
-		
-		while (numeroDigitado >= 0)
-		{
+		double soma = 0;
+		double media;
+		int contador = 0;
+
+		while (numeroDigitado >= 0) {
 			System.out.println("Digite um numero : ");
 			numeroDigitado = leia.nextInt();
-		    if (numeroDigitado >0)
-		    {
-			contador = contador + 1;
-			soma += numeroDigitado;
-			
-		    }
-		    else if (numeroDigitado ==0)
-		    {
-		    	System.out.println("O numero digitado é zero !");
-		    } 
-		    else 
-		    {
-		    	System.out.println("O numero digitado é negativo !");
-		    } 
-			
+			if (numeroDigitado > 0) {
+				contador = contador + 1;
+				soma += numeroDigitado;
+
+			} else if (numeroDigitado == 0) {
+				System.out.println("O numero digitado é zero !");
+			} else {
+				System.out.println("O numero digitado é negativo !");
+			}
+
 		}
-		
+
 		media = soma / contador;
-		System.out.print("Você digitou "+contador+" numeros validos para o calculo ");
-		System.out.print("\nA soma dos numeros que você digitou é "+soma);
-		System.out.printf("\nA média dos numeros que você digitou é %.2f",media);
-		
-		
-		
-		
-		
-		
+		System.out.print("Você digitou " + contador + " numeros validos para o calculo ");
+		System.out.print("\nA soma dos numeros que você digitou é " + soma);
+		System.out.printf("\nA média dos numeros que você digitou é %.2f", media);
+
 		leia.close();
 
 	}
--- Java/Aulas/src/lista3java/facaenquanto2.java
+++ Java/Aulas/src/lista3java/facaenquanto2.java
@@ -2,30 +2,26 @@ package lista3java;
 
 import java.util.Scanner;
 
-public class facaenquanto2
-{
+public class facaenquanto2 {
 
-	public static void main(String[] args)
-	{
-		Scanner leia = new Scanner (System.in);
-		
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+
 		int numeroDigitado;
 		int soma = 0;
 		int i = 0;
-		
+
 		System.out.println("Digite um numero :");
 		numeroDigitado = leia.nextInt();
-		
-		do
-		{
+
+		do {
 			i++;
-			soma = soma +  i;
-			
+			soma = soma + i;
+
 		} while (i < numeroDigitado);
-		
+
 		System.out.println(soma);
-		
-		
+
 		leia.close();
 
 	}
